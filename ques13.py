49a500299b7718f618ecebc70b163cde80b4da93
Bhavyareddysadula <126856102+Bhavyareddysadula@users.noreply.github.com>
2025-04-07 22:21:08 +0530

    Add files via upload

---
+++ ques13.py
@@ -0,0 +1,19 @@
+def convert(x):
+    output = []
+    for i in x:
+        temp1 = []
+        for j in i:
+            temp2 = []
+            for k in j:
+                lst = k.replace('(', '').replace(')', '')
+                new_lst = lst.split(',')
+                temp3 = []
+                for n in new_lst:
+                    temp3.append(int(n))
+                temp2.append(temp3)
+            temp1.append(temp2)
+        output.append(temp1)
+    return output
+
+print(convert([[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]))
+    
