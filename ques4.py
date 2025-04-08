322075ca9076ed6eb195734fff53c9dd0313aae2
Bhavyareddysadula <126856102+Bhavyareddysadula@users.noreply.github.com>
2025-04-08 20:35:37 +0530

    Add files via upload

---
+++ ques4.py
@@ -0,0 +1,23 @@
+import os
+
+def files(directory):
+    txt_files = []
+    for root, dir, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".txt"):
+                path = os.path.join(root,file)
+                txt_files.append(path)
+    return txt_files
+        
+def merge_files(file_list, output_path):
+    with open(output_path,'w') as output:
+        for file in file_list:
+            with open(file, 'r') as f:
+                output.write(f.read())
+                output.write('\n')
+ 
+directory = r"C:\Users\Bhavya\handson" 
+file_list = files(directory)
+output_file = os.path.join(directory , "merged.txt" )
+print(merge_files(file_list, output_file))
+    
