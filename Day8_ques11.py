d254368901c5c94d7a73e4e9b0f0e32eff517968
Bhavyareddysadula <126856102+Bhavyareddysadula@users.noreply.github.com>
2025-04-21 10:43:53 +0530

    Day8_ques11.py

--- Day8_ques11.py
+++ Day8_ques11.py
@@ -14,9 +14,9 @@ def weather(city):
     if resp_format == "xml":  #xml format http://127.0.0.1:5000/weather/ladak?format=xml
         xml_data= f"""        
         <weather>
-            <city>{city}</city>
-            <temperature>{temp}</temperature>
-            <condition>{cond}</condition>
+            <city>City : {city}</city></br>
+            <temperature>temperature: {temp}℃ </temperature></br>
+            <condition>Condition : {cond}</condition>
         </weather>
         """
         return Response(xml_data) 
@@ -24,12 +24,10 @@ def weather(city):
     #json response http://127.0.0.1:5000/weather/ladak?format=json or http://127.0.0.1:5000/weather/ladak
     json_data = { 
                     "city": city,
-                    "temperature": temp,
+                    "temperature in Celsius": temp,
                     "condition": cond
                 }                       
     return jsonify(json_data)
     
 if __name__ =='__main__':
-    app.run(debug=True)
-        
-        
+    app.run(debug = True)
