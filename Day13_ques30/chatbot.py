b2331b8f1b06f82e481a77cf84110c962fc53359
Bhavyareddysadula <126856102+Bhavyareddysadula@users.noreply.github.com>
2025-05-05 21:14:59 +0530

    Add files via upload

---
+++ Day13_ques30/chatbot.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, session
+from langchain_mistralai.chat_models import ChatMistralAI
+from langchain.schema import HumanMessage, AIMessage
+import os
+
+app = Flask(__name__)
+app.secret_key = 'mykey123'
+
+os.environ["MISTRAL_API_KEY"] = "PHhuPwA28efvLjGbbXBdfDXx0XwjKjNP" # setting up the mistral ai key to os environment
+
+llm = ChatMistralAI(model="mistral-small-latest") # chat model is mistal ai
+
+def chatbot(history):
+    messages = []
+    for msg in history:
+        if msg["role"] == "user":
+            messages.append(HumanMessage(content=msg["content"]))
+        else:
+            messages.append(AIMessage(content=msg["content"]))
+    return messages
+
+@app.route('/', methods=['GET', 'POST'])
+def chat():
+    if "history" not in session:
+        session["history"] = []
+    result = ""
+    if request.method == 'POST':
+        user_input = request.form['query']
+        session["history"].append({"role": "user", "content": user_input})
+        chat_history = chatbot(session["history"])    # for generating the response
+        response = llm(chat_history)
+        bot_reply = response.content
+        session["history"].append({"role": "assistant", "content": bot_reply})
+        session.modified = True
+        result = bot_reply
+    return render_template("chat.html", history=session["history"], result=result)
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return "Chat history cleared!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
